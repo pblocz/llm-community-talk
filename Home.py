1e77e3b13ab07eacbea4f7c32ee65e92b5eedc65
Pablo Cabeza <josepablocg@gmail.com>
2023-09-20 14:32:54 +0100

    Inital commit

---
+++ Home.py
@@ -0,0 +1,31 @@
+import streamlit as st
+
+APP_TITLE = "My Personal GPT Toolbox"
+APP_ICON = "⚙️"
+
+st.set_page_config(
+    page_title=APP_TITLE,
+    page_icon=APP_ICON,
+)
+
+st.write("# Welcome to Streamlit! 👋")
+
+st.sidebar.success("Select a demo above.")
+
+st.markdown(
+    """
+    Streamlit is an open-source app framework built specifically for
+    Machine Learning and Data Science projects.
+    **👈 Select a demo from the sidebar** to see some examples
+    of what Streamlit can do!
+    ### Want to learn more?
+    - Check out [streamlit.io](https://streamlit.io)
+    - Jump into our [documentation](https://docs.streamlit.io)
+    - Ask a question in our [community
+        forums](https://discuss.streamlit.io)
+    ### See more complex demos
+    - Use a neural net to [analyze the Udacity Self-driving Car Image
+        Dataset](https://github.com/streamlit/demo-self-driving)
+    - Explore a [New York City rideshare dataset](https://github.com/streamlit/demo-uber-nyc-pickups)
+"""
+)
